56cf663c5353e83d170117b53c4ece9ae1d038ac
lalji94 <63184003+lalji94@users.noreply.github.com>
2021-01-20 00:29:07 +0530

    Update index.js
    chanel change

--- routes/index.js
+++ routes/index.js
@@ -256,10 +256,10 @@ function postImageWidth(post_link,token,amzn_data,storeId,finalAmznData,telegrou
     }
  
  setInterval( function setup() {
-//       axios('https://t.me/s/tricksbystg03')
+      axios('https://t.me/s/tricksbystg03')
 //       axios('https://t.me/s/IHDBROADCAST')
 //       axios('https://t.me/s/IHDBroadcast')
-      axios('https://t.me/s/frcp_deals')
+//       axios('https://t.me/s/frcp_deals')
 //       axios('https://t.me/s/stg003')
       // axios('https://t.me/s/HotDealsOfficials')
         .then(response => {
